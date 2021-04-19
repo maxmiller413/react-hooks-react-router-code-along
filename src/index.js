ac641f7f912ac3f80bb1fee711700417a448f1b3
Max Miller <maxmiller413@gmail.com>
2021-04-18 20:35:54 -0500

    commit tree 65994827d3daca0d50056e437454ce7eccff6b78

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<Home />, document.getElementById("root"));
